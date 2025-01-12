1be09d46bdee9d68d8b9731bde4843a3e156eb7b
Aditya Agarwal <adityag2511@gmail.com>
2025-01-11 17:13:34 -0800

    [contextmenu][skeleton][inactive] Added basic dummy context menu options

--- src/background/background.ts
+++ src/background/background.ts
@@ -4,3 +4,52 @@ function polling() {
 }
 
 polling();
+
+// Create context menu items
+chrome.runtime.onInstalled.addListener(() => {
+  // Remove existing items to avoid duplicates
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: 'start-focus',
+      title: 'Start Focus',
+      contexts: ['action']
+    });
+
+    chrome.contextMenus.create({
+      id: 'start-short-break',
+      title: 'Start Short Break',
+      contexts: ['action']
+    });
+
+    chrome.contextMenus.create({
+      id: 'start-long-break',
+      title: 'Start Long Break',
+      contexts: ['action']
+    });
+
+    chrome.contextMenus.create({
+      id: 'view-history',
+      title: 'View History',
+      contexts: ['action']
+    });
+  });
+});
+
+// Handle menu item clicks
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case 'start-focus':
+      // TODO: Implement focus timer start
+      break;
+    case 'start-short-break':
+      // TODO: Implement short break timer start
+      break;
+    case 'start-long-break':
+      // TODO: Implement long break timer start
+      break;
+    case 'view-history':
+      // TODO: Open history view
+      chrome.runtime.openOptionsPage();
+      break;
+  }
+});
